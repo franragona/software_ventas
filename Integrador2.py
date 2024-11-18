2e2770f41edf7580c8d83c7e44323254c9d0d423
Franco Ragona <62776791+franragona@users.noreply.github.com>
2024-11-18 16:02:51 -0300

    Correccion Integrador2.py

--- Integrador2.py
+++ Integrador2.py
@@ -6,80 +6,77 @@ fecha_hora_actual = datetime.datetime.now()
 # Formatear la fecha y hora
 formato = fecha_hora_actual.strftime("%a %b %d %H:%M:%S %Y")
 
-
 def solo_letras(mensaje):
     while True:
         nombre = input(mensaje)
         if nombre.isalpha():
             return nombre.capitalize()
         else:
-            print("Por favor solo utilize letras de A hasta Z.")
-            
+            print("Por favor, utilice solo letras de A hasta Z.")
+
 def solo_numeros(mensaje):
     while True:
         try:
             ingreso = int(input(mensaje))
             return ingreso
         except ValueError:
-            print("Opcion incorrecta.")
-            
+            print("Opción incorrecta. Por favor, ingrese un número válido.")
+
 def pedido():
-    
     cliente = solo_letras("Ingrese nombre del cliente: ")
-    combo_s = solo_numeros("Ingrese cantidad Combo S : ")
-    combo_d = solo_numeros("Ingrese cantidad Combo D : ")
-    combo_t = solo_numeros("Ingrese cantidad Combo T : ")
-    flurby = solo_numeros("Ingrese cantidad Flurby : ")
-    
+    combo_s = solo_numeros("Ingrese cantidad Combo S: ")
+    combo_d = solo_numeros("Ingrese cantidad Combo D: ")
+    combo_t = solo_numeros("Ingrese cantidad Combo T: ")
+    flurby = solo_numeros("Ingrese cantidad Flurby: ")
+
     total = (combo_s * 5) + (combo_d * 6) + (combo_t * 7) + (flurby * 2)
-    print(f"Total ${total}")
+    print(f"Total: ${total}")
     while True:
         abona = solo_numeros("Abona con $")
         vuelto = abona - total
         if vuelto >= 0:
-            print(f"Vuelto ${vuelto}")
+            print(f"Vuelto: ${vuelto}")
             break
         else:
             print("El monto no es suficiente. Por favor, ingrese un monto mayor o igual al total.")
-    
+
     while True:
-        confirmar = solo_letras("¿Confirma pedido? Y/N : ")
+        confirmar = solo_letras("¿Confirma pedido? Y/N: ")
         if confirmar == "Y":
-            with open("ventas.txt","a") as f:
+            with open("ventas.txt", "a") as f:
                 f.write(f"{cliente};{formato};Combo S:{combo_s};Combo D:{combo_d};Combo T:{combo_t};Flurby:{flurby};Total:{total}\n")
             return total
         elif confirmar == "N":
-            print("Bueno andate a la concha de tu madre")
+            print("El pedido ha sido cancelado.")
             return 0
         else:
-            print("Y si escribimos Y o N la puta que te pario no sabes leer?")
-            
+            print("Por favor, ingrese 'Y' para confirmar o 'N' para cancelar.")
+
 def ingreso():
-    with open("registro.txt","a") as f:
+    with open("registro.txt", "a") as f:
         f.write(f"IN;{formato};Encargad@ {nombre}\n")
 
 def salida():
-    with open("registro.txt","a") as f:
-        f.write(f"OUT;{formato};Encargad@ {nombre}; Caja:${caja}\n" + "#"*50 + "\n")
+    with open("registro.txt", "a") as f:
+        f.write(f"OUT;{formato};Encargad@ {nombre}; Caja: ${caja}\n" + "#" * 50 + "\n")
 
 ejecutar_programa = True
 
 while ejecutar_programa:
-    
     caja = 0
-    
-    #Introduccion
+
+    # Introducción
     print("Bienvenido a Hamburguesas IT")
 
-    #Ingreso encargado
+    # Ingreso encargado
     nombre = solo_letras("Ingrese su nombre encargad@: ")
     if nombre:
         ingreso()
 
-    #Menu
-    print(f"Hamburguesas IT\nEncargad@ -> {nombre}\nRecuerda, siempre hay que recibir al cliente con una sonrisa :)")
+    # Menú
+    print(f"Hamburguesas IT\nEncargad@ -> {nombre}\nRecuerde, siempre hay que recibir al cliente con una sonrisa :)")
     while True:
-        #Opciones
+        # Opciones
         opcion = solo_numeros("1 – Ingreso nuevo pedido\n2 – Cambio de turno\n3 – Apagar sistema\n>>> ")
 
         if opcion == 1:
@@ -92,4 +89,3 @@ while ejecutar_programa:
             print("Apagando sistema...")
             ejecutar_programa = False
             break
-    
